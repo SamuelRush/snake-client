5737a59274dfb58cc2aa93901f80ade16105cde7
Sam <samuelrush@gmail.com>
2019-10-23 20:14:40 +0000

    added the ability for user input and for control C out of code

--- play.js
+++ play.js
@@ -6,3 +6,17 @@ const net = require('net');
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = stdin.on('data', (data) => { //if control c is entered, exit
+    if (data === "\u0003") {
+      process.exit();
+    }
+  })
+  return stdin;
+};
+setupInput();
